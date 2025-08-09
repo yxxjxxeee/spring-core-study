802d073be663ee4fc90aba8e55adb6759644a900
YJE <c0d1ngyje@gmail.com>
2025-08-10 02:03:29 +0900

    ♻️ AppConfig에서 객체 생성 관리 개선

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,20 +1,24 @@
 package hello.core;
 
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
-import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import hello.core.member.MemoryMemberRepository;
-import hello.core.order.OrderService;
-import hello.core.order.OrderServiceImpl;
+import hello.core.discount.*;
+import hello.core.member.*;
+import hello.core.order.*;
 
 public class AppConfig {
 
   public MemberService memberService() {
-    return new MemberServiceImpl(new MemoryMemberRepository());
+    return new MemberServiceImpl(memberRepository());
   }
 
   public OrderService orderService() {
-    return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+    return new OrderServiceImpl(memberRepository(), discountPolicy());
+  }
+
+  private static MemberRepository memberRepository() {
+    return new MemoryMemberRepository();
+  }
+
+  public static DiscountPolicy discountPolicy() {
+    return new FixDiscountPolicy();
   }
 }

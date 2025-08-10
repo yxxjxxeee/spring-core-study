b7e5f7cc5d4230e430caa1fff44c22e1a31c674d
YJE <c0d1ngyje@gmail.com>
2025-08-10 13:06:28 +0900

    ♻️ 스프링 컨테이너 기반 의존성 주입 방식으로 변경

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -1,23 +1,35 @@
 package hello.core;
 
-import hello.core.discount.*;
-import hello.core.member.*;
-import hello.core.order.*;
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
 
+  @Bean
   public MemberService memberService() {
     return new MemberServiceImpl(memberRepository());
   }
 
+  @Bean
   public OrderService orderService() {
     return new OrderServiceImpl(memberRepository(), discountPolicy());
   }
 
-  private static MemberRepository memberRepository() {
+  @Bean
+  public static MemberRepository memberRepository() {
     return new MemoryMemberRepository();
   }
 
+  @Bean
   public static DiscountPolicy discountPolicy() {
     return new RateDiscountPolicy();
   }
